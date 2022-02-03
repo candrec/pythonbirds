6751ffb1a7f2702669f358eec231db39f5c50675
andrealmeida <almeidacandrec@gmail.com>
2022-02-03 10:15:15 -0300

    Criada metodo cumprimentar

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,8 @@
 class Pessoa:
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return 'Olá'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
-    print(id(p))
     print(p.cumprimentar())
